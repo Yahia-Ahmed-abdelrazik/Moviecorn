55a08666cfda87240bd41318023c03b18405dd77
Yahia-Ahmed-abdelrazik <otakayahia@gmail.com>
2024-06-27 20:34:03 +0300

    app-v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import StarRating from "./StarRating.jsx";
+
+function Rdd() {
+  const [movierating, setMovieRating] = useState(0);
+
+  return (
+    <>
+      <StarRating
+        maxRating={10}
+        defultRating={3}
+        onSetRating={setMovieRating}
+      />
+      {movierating}
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <Rdd />
+    <StarRating maxRating={10} />
+
+    <StarRating maxRating={10} defultRating={1} />
+    <StarRating maxRating={10} defultRating={7} />
+    <StarRating maxRating={10} defultRating={5} />
+    <StarRating maxRating={10} defultRating={11} />
+    <StarRating maxRating={10} />
+  </React.StrictMode>
+);
